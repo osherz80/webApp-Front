843a66c2f4871192e6ee998628c163e90c3ef5dd
Assaf Revacha <assaf.git.arm@gmail.com>
2026-04-07 15:48:21 +0300

    add edit form to edit dialog

--- src/components/feed/EditPostDialog.tsx
+++ src/components/feed/EditPostDialog.tsx
@@ -4,37 +4,61 @@ import {
     DialogTitle,
     DialogContent,
     DialogActions,
-    TextField,
-    Button
+    Button,
+    CircularProgress
 } from '@mui/material';
 import type { Post } from '../../types/post';
+import EditPostForm from './EditPostForm';
 
 interface EditPostDialogProps {
     post: Post | null;
     message: string;
+    editImageFile: File | null;
+    removeImage: boolean;
+    isUpdating: boolean;
     onClose: () => void;
     onMessageChange: (val: string) => void;
+    onImageChange: (file: File | null) => void;
+    onRemoveImageChange: (remove: boolean) => void;
     onUpdate: () => void;
 }
 
-const EditPostDialog: React.FC<EditPostDialogProps> = ({ post, message, onClose, onMessageChange, onUpdate }) => {
+const EditPostDialog: React.FC<EditPostDialogProps> = ({ 
+    post, 
+    message, 
+    editImageFile, 
+    removeImage,
+    isUpdating, 
+    onClose, 
+    onMessageChange, 
+    onImageChange, 
+    onRemoveImageChange,
+    onUpdate 
+}) => {
     return (
         <Dialog open={!!post} onClose={onClose} fullWidth maxWidth="xs">
             <DialogTitle>Update Recommendation</DialogTitle>
             <DialogContent>
-                <TextField
-                    fullWidth
-                    multiline
-                    rows={4}
-                    value={message}
-                    onChange={(e) => onMessageChange(e.target.value)}
-                    sx={{ mt: 1 }}
-                    autoFocus
+                <EditPostForm
+                    message={message}
+                    editImageFile={editImageFile}
+                    existingImageUrl={post?.userImage}
+                    removeImage={removeImage}
+                    onMessageChange={onMessageChange}
+                    onImageChange={onImageChange}
+                    onRemoveImageChange={onRemoveImageChange}
                 />
             </DialogContent>
             <DialogActions>
-                <Button onClick={onClose}>Cancel</Button>
-                <Button onClick={onUpdate} variant="contained">Update</Button>
+                <Button onClick={onClose} disabled={isUpdating}>Cancel</Button>
+                <Button 
+                    onClick={onUpdate} 
+                    variant="contained" 
+                    disabled={isUpdating}
+                    startIcon={isUpdating ? <CircularProgress size={20} /> : undefined}
+                >
+                    {isUpdating ? 'Updating...' : 'Update'}
+                </Button>
             </DialogActions>
         </Dialog>
     );
