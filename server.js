46485488d2a6b02411e820d690fa89d32dcd89c0
Osher <osherzohar80@gmail.com>
2026-04-22 20:57:10 +0300

    change to modules

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
 const PORT = 80;
+
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
